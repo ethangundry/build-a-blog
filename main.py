8708d778358e88262dedc9fccaef92e1b7368f7a
Ethan Gundry <gundryethan@gmail.com>
2017-07-28 16:48:05 -0700

    Made a blog

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,20 +10,54 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:lc101@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 
-db = SQLALCHEMY(app)
+db = SQLAlchemy(app)
 #sets the database
 
-class Blog(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-    
-	name = db.Column(db.String(120))
-	#this column is called Name which is a string that has a max length of 120 characters
-	body = db.Column(db.String(120))
-	#this column is called Body which is a string that has a max length of 120 characters
+app.secret_key = 'A0Zr98j/2yX R~XHH!jmN]LWX/,?RT'
+#sets the secret key
 
-	def __init__(self, name, body):
-		self.name = name
-		self.body = body
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+    name = db.Column(db.String(120))
+    #this column is called Name which is a string that has a max length of 120 characters
+    body = db.Column(db.String(120))
+    #this column is called Body which is a string that has a max length of 120 characters   
+
+    def __init__(self, name, body):
+        self.name = name
+        self.body = body
+
+posts = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    posts = Blogpost.query.all()
+    return render_template("blog.html", title="Build-A-Blog", posts=posts)
+
+@app.route('/blog')
+def blog():
+    post_id = request.args.get('id')
+    post = Blogpost.query.get(post_id)
+    return render_template("blogpost.html", title="Build-A-Blog", post=post)
+
+@app.route('/newpost', methods=['GET', 'POST'])
+def newpost():
+
+    if request.method == 'POST':
+        name = request.form['name']
+        body = request.form['body']
+
+        post = Blogpost(name, body)
+        db.session.add(post)
+        db.session.commit()
+        
+        #redirect to index if succesful
+        return redirect(url_for("blog", id=post.id))
+
+    else :
+        return render_template("newpost.html",title="New Post")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
